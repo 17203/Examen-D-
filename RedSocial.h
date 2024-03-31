c742f90ed381a7e3378424bffbaf70d9ee3dadd6
17203 <17203@portalcedi.edu.mx>
2024-03-30 20:21:24 -0600

    a

--- RedSocial.h
+++ RedSocial.h
@@ -17,5 +17,23 @@ class RedSocial{
     void mostrarUsuario();
     void mostrarPublicacion();
     Usuario* getUsuario (int id);
+}
 
 #endif
+
+/*Métodos públicos:
+int getid
+Regresa el id del Usuario.
+void mostrar
+Muestra los datos del usuario, incluyendo el ID del usuario.
+void mostrarAmigos
+Muestra una lista de todos los amigos del usuario.
+void mostrarPublicaciones
+■Muestra todas las publicaciones que ha hecho el usuario.
+void agregarAmigo (Usuario* nuevoAmigo)
+Agrega un usuario existente de la Red Social como amigo.
+■ Al agregar un Usuario como amigo, ambos Usuarios se vuelven amigos, es decir Usuario A está en la lista de amigos de Usuario B y Usuario B está en la lista de amigos del Usuario A
+void crearPublicacion
+Crea una nueva publicación para el usuario.
+■ Crea la publicación directamente desde el método y agrega el apuntador de la publicación al listado de publicaciones del usuario.
+Usuario* getAmigo(int id)*/
